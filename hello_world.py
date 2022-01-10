0bfd73317eed3f53b956d434521823f4e3f5f0e8
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 15:52:58 -0500

    Finished the project.

--- hello_world.py
+++ hello_world.py
@@ -9,8 +9,8 @@ name = 42
 print("Hello", str(42))	# with a comma
 print("Hello " + str(42))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
-#fave_food1 = "sushi"
-#fave_food2 = "pizza"
-#print( your code here ) # with .format()
-#print( your code here ) # with an f string
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1, fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
 
