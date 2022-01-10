0e48dbdfd2caaff25d727e97b050aa781eff0201
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 15:41:19 -0500

    Print Hello Noelle.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
-#name = "Noelle"
-#print( your code here )	# with a comma
-#print( your code here )	# with a +
+name = "Noelle"
+print("Hello", name)	# with a comma
+print("Hello" + " " + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
 #name = 42
 #print( your code here )	# with a comma
