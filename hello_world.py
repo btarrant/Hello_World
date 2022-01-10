af04de9d8d5a0f66b4ee3cd7cdf8fa8b14caed90
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-10 15:47:13 -0500

    Print Hello 42.

--- hello_world.py
+++ hello_world.py
@@ -5,9 +5,9 @@ name = "Noelle"
 print("Hello", name)	# with a comma
 print("Hello" + " " + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
-#name = 42
-#print( your code here )	# with a comma
-#print( your code here )	# with a +	-- this one should give us an error!
+name = 42
+print("Hello", str(42))	# with a comma
+print("Hello " + str(42))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 #fave_food1 = "sushi"
 #fave_food2 = "pizza"
